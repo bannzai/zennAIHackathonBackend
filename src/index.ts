d06799fa969ee1359e2eecb3dcb0a721502dc5d8
bannzai <kingkong999yhirose@gmail.com>
2025-01-20 01:19:08 +0900

    WIP index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from "express";
+import admin from 'firebase-admin';
 
 const app: Application = express();
 
@@ -10,6 +11,20 @@ app.get("/", async (req: Request, res: Response) => {
     message: "Hello World!",
   });
 });
+
+// Firebase Admin SDKの初期化
+const serviceAccountKeyPath = process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH;
+if (!serviceAccountKeyPath) {
+  console.error("環境変数 FIREBASE_SERVICE_ACCOUNT_KEY_PATH が設定されていません。");
+  process.exit(1);
+}
+
+const serviceAccount = require(serviceAccountKeyPath);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+console.log("Firebase Admin SDK initialized.");
 
 const port = process.env.PORT || 8000;
 try {
