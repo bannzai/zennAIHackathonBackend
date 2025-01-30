2dcb97465d13b2ca9584ab5feb22c6779a4a3206
bannzai <kingkong999yhirose@gmail.com>
2025-01-30 12:00:25 +0900

    Format

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,26 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-import { genkit } from 'genkit';
-import { gemini15Flash, vertexAI } from '@genkit-ai/vertexai';
+import { genkit } from "genkit";
+import { gemini15Flash, vertexAI } from "@genkit-ai/vertexai";
 
 const ai = genkit({
-    plugins: [
-        vertexAI({ location: 'us-central1' }),
-    ],
+  plugins: [vertexAI({ location: "us-central1" })],
 });
 
-app.get('/', async (req, res) => {
-    const animal = req.query.animal || 'dog';
-    const prompt = `Give me 10 fun facts about ${animal}. Return this as html without backticks.`
-    const llmResponse = await ai.generate({
-        model: gemini15Flash,
-        prompt: prompt,
-    });
-    const html = llmResponse.text;
-    res.send(html);
+app.get("/", async (req, res) => {
+  const animal = req.query.animal || "dog";
+  const prompt = `Give me 10 fun facts about ${animal}. Return this as html without backticks.`;
+  const llmResponse = await ai.generate({
+    model: gemini15Flash,
+    prompt: prompt,
+  });
+  const html = llmResponse.text;
+  res.send(html);
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`codelab-genai: listening on port ${port}`);
+  console.log(`codelab-genai: listening on port ${port}`);
 });
