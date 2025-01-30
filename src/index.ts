b58d7e68cf9bceb1d846e8eda4fe357e41432c59
bannzai <kingkong999yhirose@gmail.com>
2025-01-30 23:52:04 +0900

    Fix

--- src/index.ts
+++ src/index.ts
@@ -37,15 +37,15 @@ export const askForIngredientsFlow = ai.defineFlow(
     inputSchema: z.string(),
     outputSchema: z.string(),
   },
-  async (food: string) => {
+  async (question: string) => {
     const model = genAI.getGenerativeModel({
       model: "gemini-1.5-flash",
       tools: [
         {
           googleSearchRetrieval: {
             dynamicRetrievalConfig: {
-              mode: DynamicRetrievalMode.MODE_UNSPECIFIED,
-              dynamicThreshold: 0.5,
+              // mode: DynamicRetrievalMode.MODE_DYNAMIC,
+              // dynamicThreshold: 0.5,
             },
           },
         },
@@ -53,14 +53,35 @@ export const askForIngredientsFlow = ai.defineFlow(
     });
     // フロー内でLLMの実行やその他ワークフロー処理を行う
 
-    const result = await model.generateContent(
-      `${food} の材料を日本語で教えて下さい.`
-    );
+    const result = await model.generateContent(question);
     const output = result.response?.text();
-    const metadata =
-      result.response?.candidates?.[0]?.groundingMetadata
-        ?.groundingChuncks?.[0];
-    console.log(metadata);
+
+    console.log("grouding---");
+    const response = result.response;
+    const candidates = response?.candidates;
+    const groudingMetadata = candidates?.[0]?.groundingMetadata;
+    const anyGroudingMetadata = groudingMetadata as any;
+    // typo: https://github.com/google-gemini/generative-ai-js/issues/323
+    const groundingChunks = anyGroudingMetadata["groundingChunks"];
+    const web = groundingChunks?.[0].web;
+    const title = web?.title;
+    const url = web?.uri;
+    // console.log(JSON.stringify({ response: response }, null, 2));
+    // console.log(JSON.stringify({ candidates: candidates }, null, 2));
+    // console.log(
+    //   JSON.stringify({ groudingMetadata: groudingMetadata }, null, 2)
+    // );
+    // console.log(
+    //   JSON.stringify({
+    //     groundingChunksIsNull: groundingChunks === null,
+    //     groudingChunksIsUndefined: groundingChunks === undefined,
+    //     groundingChunksIsArray: Array.isArray(groundingChunks),
+    //   })
+    // );
+    console.log(JSON.stringify({ groundingChunks: groundingChunks }));
+    console.log(JSON.stringify({ web: web }, null, 2));
+    console.log(JSON.stringify({ title: title }, null, 2));
+    console.log(JSON.stringify({ url: url }, null, 2));
 
     // const { output } = await ai.generate({
     //   model: gemini15Flash,
