b46f5c856c0460b03aa3b7d5ff381e888987f676
bannzai <kingkong999yhirose@gmail.com>
2025-02-01 06:07:58 +0900

    export googleSearchTool

--- src/utils/ai/ai.ts
+++ src/utils/ai/ai.ts
@@ -19,15 +19,15 @@ export const googleGenerativeAI = new GoogleGenerativeAI(
   process.env.GEMINI_API_KEY!
 );
 
-export function googleSearchModel(): GenerativeModel {
-  // Unable to submit request because Please use google_search field instead of google_search_retrieval field.. Learn more: https://cloud.google.com/vertex-ai/generative-ai/docs/model-reference/gemini
-  // gemini-2.0-flash-exp では google_search_retrieval が使えない googleSearch を無理やり渡せば使える
-  // なお、今の所コスト面以外では gemini-1.5-flashにして googleSearchRetrival を使っても問題はない
-  // https://stackoverflow.com/questions/79289711/grounding-with-google-search-with-gemini-2-0-flash-exp
-  const googleSearchTool = {
-    googleSearch: {},
-  } as Tool;
+// Unable to submit request because Please use google_search field instead of google_search_retrieval field.. Learn more: https://cloud.google.com/vertex-ai/generative-ai/docs/model-reference/gemini
+// gemini-2.0-flash-exp では google_search_retrieval が使えない googleSearch を無理やり渡せば使える
+// なお、今の所コスト面以外では gemini-1.5-flashにして googleSearchRetrival を使っても問題はない
+// https://stackoverflow.com/questions/79289711/grounding-with-google-search-with-gemini-2-0-flash-exp
+export const googleSearchTool = {
+  googleSearch: {},
+} as Tool;
 
+export function googleSearchModel(): GenerativeModel {
   // const model = googleGenerativeAI.getGenerativeModel({
   //   model: "gemini-1.5-flash",
   //   tools: [
