f4226d0abdb1c3fda84653aae77c213855cf2d5d
bannzai <kingkong999yhirose@gmail.com>
2025-01-31 13:24:15 +0900

    Fix

--- src/flows/todoCreate/flow.ts
+++ src/flows/todoCreate/flow.ts
@@ -1,7 +1,12 @@
 import { GenerateOptions, genkit, z } from "genkit";
-import { genkitAI, googleGenerativeAI } from "../../utils/ai/ai";
+import {
+  genkitAI,
+  googleGenerativeAI,
+  googleSearchModel,
+} from "../../utils/ai/ai";
 import { SchemaType } from "@google/generative-ai";
 import { ToolDefinition } from "genkit/model";
+import zodToJsonSchema from "zod-to-json-schema";
 
 const TODOCreateSchemaInput = z.object({
   question: z.string(),
@@ -10,6 +15,7 @@ const TODOCreateSchemaInput = z.object({
 const TODOCreateSchemaOutput = z.array(
   z.object({
     content: z.string(),
+    url: z.string(),
   })
 );
 
@@ -30,14 +36,21 @@ export const todoCreateFlow = genkitAI.defineFlow(
   {
     name: "todoCreateFlow",
     inputSchema: TODOCreateSchemaInput,
-    outputSchema: TODOCreateSchemaOutput,
+    outputSchema: z.string(),
   },
   async (input) => {
-    const options: GenerateOptions = {
-      prompt: `${input.question} を達成するために必要なTODOリストを教えてください`,
-      output: { schema: TODOCreateSchemaOutput },
-    };
-    const response = await genkitAI.generate(options);
-    return response.output;
+    // const options: GenerateOptions = {
+    //   prompt: `${input.question} を達成するために必要なTODOリストを教えてください`,
+    //   output: { schema: TODOCreateSchemaOutput },
+    // };
+    // const response = await genkitAI.generate(options);
+
+    const model = googleSearchModel();
+    const schema = zodToJsonSchema(TODOCreateSchemaOutput);
+    console.log(JSON.stringify({ schema: schema }, null, 2));
+    const result = await model.generateContent(
+      `${"結婚に必要なこと"} を達成するために必要なTODOリストを出力してください。JSON形式で出力してください。スキーマは ${TODOCreateSchemaOutput.toString()} です。`
+    );
+    return result.response?.text() ?? "";
   }
 );
