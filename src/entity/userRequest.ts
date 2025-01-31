717097dc414268b1d43e47c661d4812e5ef31533
bannzai <kingkong999yhirose@gmail.com>
2025-01-31 18:50:57 +0900

    fix

--- src/entity/userRequest.ts
+++ src/entity/userRequest.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 export const UserRequestSchema = z.object({
   userID: z.string(),
   requestDateTime: z.date(),
-  request: z.string(),
 });
 
 export type UserRequest = z.infer<typeof UserRequestSchema>;
