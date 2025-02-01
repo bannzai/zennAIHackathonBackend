0112c911c8465a468d7e925e4f7044decbd6572c
bannzai <kingkong999yhirose@gmail.com>
2025-02-01 15:40:29 +0900

    fix index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
-import { genkitAI } from "./utils/ai/ai";
+import { z } from "zod";
+import { genkitAI, googleSearchGroundingData } from "./utils/ai/ai";
 
 export const taskCreate = require("./flows/taskCreate/flow");
+
+export const test = genkitAI.defineFlow(
+  {
+    name: "askForIngredientsFlow",
+    inputSchema: z.string(),
+    outputSchema: z.string(),
+  },
+  async (question: string) => {
+    const { aiTextResponse, groundings } = await googleSearchGroundingData(
+      question
+    );
+
+    if (!aiTextResponse) {
+      throw new Error("Failed to generate ingredients");
+    }
+    return aiTextResponse;
+  }
+);
 
 genkitAI.startFlowServer({ flows: [taskCreate] });
