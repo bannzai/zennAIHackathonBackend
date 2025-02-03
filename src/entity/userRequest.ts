f02c24b7dc7ddd714dd023ed3a0d5d991b1e03c0
bannzai <kingkong999yhirose@gmail.com>
2025-02-03 12:53:25 +0900

    Remove requestDateTime

--- src/entity/userRequest.ts
+++ src/entity/userRequest.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 
 export const UserRequestSchema = z.object({
   userID: z.string(),
-  requestDateTime: z.date(),
 });
 
 export type UserRequest = z.infer<typeof UserRequestSchema>;
