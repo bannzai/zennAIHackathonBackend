8bbdce63d280fd68f6636635f13f16a54cec4c00
bannzai <kingkong999yhirose@gmail.com>
2025-02-04 09:09:01 +0900

    Fix

--- src/entity/todo.ts
+++ src/entity/todo.ts
@@ -5,8 +5,8 @@ export const TODOSchema = z.object({
   id: z.string(),
   userID: z.string(),
   taskID: z.string(),
-  content: z.string(),
-  supplement: z.string().optional(),
+  content: z.string().describe("TODOの内容"),
+  supplement: z.string().describe("補足情報"),
   aiTextResponse: z.string(),
   groundings: z.array(GroundingDataSchema),
 });
