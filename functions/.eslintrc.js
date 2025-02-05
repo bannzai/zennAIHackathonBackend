c9196514c8ae3291ab426a646a1f35ce03884ede
bannzai <kingkong999yhirose@gmail.com>
2025-02-05 09:35:39 +0900

    Overwrite .eslintrc.js and tsconfig.json

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,33 +1,21 @@
 module.exports = {
-  root: true,
   env: {
-    es6: true,
+    browser: true,
     node: true,
+    es2021: true,
   },
   extends: [
     "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
     "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
+    ecmaVersion: "latest",
   },
-  ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
-    "/generated/**/*", // Ignore generated files.
-  ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: ["*.test.ts", "*.test.js", "lib"],
   rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
-    "indent": ["error", 2],
+    "@typescript-eslint/no-explicit-any": "off",
   },
 };
