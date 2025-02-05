a0955ea6e078b7dcc63dca1e034097f26c5d688c
bannzai <kingkong999yhirose@gmail.com>
2025-02-05 10:01:40 +0900

    Add input and task

---
+++ functions/src/functions/taskCreate/task.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+import { onTaskDispatched } from "firebase-functions/tasks";
+import { taskCreate } from "./flow";
+import { TaskCreateSchema } from "./input";
+
+export const executeTaskCreate = onTaskDispatched(
+  {
+    retryConfig: {
+      maxAttempts: 5,
+      minBackoffSeconds: 60,
+    },
+    rateLimits: {
+      maxConcurrentDispatches: 6,
+    },
+  },
+  async (req) => {
+    console.log("#executeTaskCreate");
+    const task = req.data as z.infer<typeof TaskCreateSchema>;
+    const result = await taskCreate(task);
+    // TODO: Change state `Task` to `Done`;
+    return;
+  }
+);
