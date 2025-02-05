5a5074a1e7e6d760f81f35a5795892a6b886b70e
bannzai <kingkong999yhirose@gmail.com>
2025-02-05 12:10:46 +0900

    Revert "Add localhost code"
    This reverts commit fdd7ac3bc15b0d00d0b453da104963746524488a.

--- functions/src/utils/firebase/gcp.ts
+++ functions/src/utils/firebase/gcp.ts
@@ -3,27 +3,25 @@ import { GoogleAuth } from "google-auth-library";
 
 // ref: https://firebase.google.com/docs/functions/task-functions?gen=2nd
 export async function getFunctionURL(name: string): Promise<string> {
-  if (process.env.APP_ENV === "local") {
-    return `http://localhost:3400/${name}`;
-  } else {
-    const auth = new GoogleAuth({
-      scopes: "https://www.googleapis.com/auth/cloud-platform",
-    });
-    const projectId = await auth.getProjectId();
+  const auth = new GoogleAuth({
+    scopes: "https://www.googleapis.com/auth/cloud-platform",
+  });
+  const projectId = await auth.getProjectId();
 
-    // NOTE: [CloudTask:Region] "asia-northeast1" を使いたいが、なぜか us-central1 じゃないと動かないため明示的に指定する
-    const location = "us-central1";
-    const url = `https://cloudfunctions.googleapis.com/v2beta/projects/${projectId}/locations/${location}/functions/${name}`;
+  // NOTE: [CloudTask:Region] "asia-northeast1" を使いたいが、なぜか us-central1 じゃないと動かないため明示的に指定する
+  const location = "us-central1";
+  const url =
+    "https://cloudfunctions.googleapis.com/v2beta/" +
+    `projects/${projectId}/locations/${location}/functions/${name}`;
 
-    functions.logger.log(`Getting function URL for ${name} at ${url}`);
+  functions.logger.log(`Getting function URL for ${name} at ${url}`);
 
-    const client = await auth.getClient();
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const res = await client.request<any>({ url });
-    const uri = res.data?.serviceConfig?.uri;
-    if (!uri) {
-      throw new Error(`Unable to retreive uri for function at ${url}`);
-    }
-    return uri;
+  const client = await auth.getClient();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const res = await client.request<any>({ url });
+  const uri = res.data?.serviceConfig?.uri;
+  if (!uri) {
+    throw new Error(`Unable to retreive uri for function at ${url}`);
   }
+  return uri;
 }
