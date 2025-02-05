4397d55ab3c227addbec555af510888876fa8a0c
bannzai <kingkong999yhirose@gmail.com>
2025-02-05 10:18:07 +0900

    Add enqueueTask

---
+++ functions/src/functions/taskCreate/enqueue_task.ts
@@ -0,0 +1,46 @@
+import { z } from "zod";
+import { taskCreate } from "./flow";
+import { TaskCreateSchema } from "./input";
+import { genkitAI } from "../../utils/ai/ai";
+import { DataResponseSchema, ErrorResponseSchema } from "../../entity/response";
+import { getFunctions } from "firebase-admin/functions";
+import { getFunctionURL } from "../../utils/firebase/gcp";
+import { authMiddleware } from "../../middleware/authMiddleware";
+
+const ResponseSchema = z.union([
+  DataResponseSchema.extend({
+    data: z.object({
+      taskID: z.string(),
+    }),
+  }),
+  ErrorResponseSchema,
+]);
+
+export const enqueueTaskCreate = genkitAI.defineFlow(
+  {
+    name: "enqueueTaskCreate",
+    inputSchema: TaskCreateSchema,
+    outputSchema: ResponseSchema,
+    middleware: [authMiddleware],
+  },
+  async (input) => {
+    const queue = getFunctions().taskQueue("executeTaskCreate");
+    const executeTaskCreateURL = await getFunctionURL("executeTaskCreate");
+    queue.enqueue(input, {
+      uri: executeTaskCreateURL,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const response: z.infer<typeof ResponseSchema> = {
+      result: "OK",
+      statusCode: 200,
+      data: {
+        taskID: "123",
+      },
+    };
+
+    return response;
+  }
+);
