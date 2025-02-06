6e182fe4b4ca8a7156d8b45ca630a42f0964967a
bannzai <kingkong999yhirose@gmail.com>
2025-02-06 19:02:34 +0900

    Add zod typeguard utility

---
+++ functions/src/utils/stdlib/type_guard.ts
@@ -0,0 +1,37 @@
+import type z from "zod";
+
+// 実際に遭遇したケース。safeParseでは、指定した型の新しいparseされたインスタンスが返却されるのでtype guardを用意する
+//
+// export const TaskPreparedSchema = z
+//   .object({
+//     status: z.literal("prepared"),
+//     fullFilledDateTime: FirestoreTimestampSchema,
+//   })
+// .....
+//   const updateTask: z.infer<typeof updateTaskSchema> = {
+//     status: "prepared",
+//     fullFilledDateTime: Timestamp.now(),
+//     serverUpdatedDateTime: Timestamp.now(),
+//   };
+
+//   const taskSnapshot = await database
+//     .doc(`/users/${userID}/tasks/${taskID}`)
+//     .get();
+//   const task = TaskPreparedSchema.safeParse({
+//     ...(taskSnapshot.data() ?? {}),
+//     ...updateTask,
+//   }).data;
+//   if (!task) {
+//     // 用意できなかったプロパティがあると判断するのでRetryする
+//     functions.logger.info("task not fullfilled", {
+//       taskLoading,
+//     });
+//     throw new Error("task not fullfilled");
+//   }
+
+export function zodTypeGuard<T extends z.ZodTypeAny>(
+  schema: T,
+  data: unknown
+): data is z.infer<T> {
+  return schema.safeParse(data).success;
+}
