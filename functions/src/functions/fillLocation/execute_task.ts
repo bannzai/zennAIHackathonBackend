01b557a1ac264be6cd0d555d548c655865cb1dca
bannzai <kingkong999yhirose@gmail.com>
2025-02-09 17:15:04 +0900

    Refactor

--- functions/src/functions/fillLocation/execute_task.ts
+++ functions/src/functions/fillLocation/execute_task.ts
@@ -2,10 +2,10 @@ import * as functions from "firebase-functions";
 import { z } from "zod";
 import { onTaskDispatched } from "firebase-functions/tasks";
 import { fillTaskLocation, fillTODOLocation } from "./flow";
-import { FillLocationSchema } from "./input";
+import { FillLocationSchema, FillTODOLocationSchema } from "./input";
 import { errorMessage } from "../../utils/error/message";
 
-export const executeFillLocation = onTaskDispatched(
+export const executeFillTaskLocation = onTaskDispatched(
   {
     retryConfig: {
       maxAttempts: 3,
@@ -18,12 +18,35 @@ export const executeFillLocation = onTaskDispatched(
     memory: "1GiB",
   },
   async (req) => {
-    console.log("#executeFillLocation");
+    console.log("#executeFillTaskLocation");
     try {
       const input = req.data as z.infer<typeof FillLocationSchema>;
       const fillTaskLocationResponse = await fillTaskLocation(input);
+      console.log({ fillTaskLocationResponse });
+    } catch (err) {
+      functions.logger.error(errorMessage(err));
+    }
+  }
+);
+
+export const executeFillTODOLocation = onTaskDispatched(
+  {
+    retryConfig: {
+      maxAttempts: 3,
+      minBackoffSeconds: 60,
+    },
+    rateLimits: {
+      maxConcurrentDispatches: 3,
+    },
+    timeoutSeconds: 10 * 60,
+    memory: "1GiB",
+  },
+  async (req) => {
+    console.log("#executeFillTODOLocation");
+    try {
+      const input = req.data as z.infer<typeof FillTODOLocationSchema>;
       const fillTODOLocationResponse = await fillTODOLocation(input);
-      console.log({ fillTaskLocationResponse, fillTODOLocationResponse });
+      console.log({ fillTODOLocationResponse });
     } catch (err) {
       functions.logger.error(errorMessage(err));
     }
--- functions/src/functions/fillLocation/flow.ts
+++ functions/src/functions/fillLocation/flow.ts
@@ -1,6 +1,5 @@
 import { z } from "genkit";
-import { genkitAI, googleSearchGroundingData } from "../../utils/ai/ai";
-import { TODOSchema } from "../../entity/todo";
+import { genkitAI } from "../../utils/ai/ai";
 import { database } from "../../utils/firebase/firebase";
 import {
   DataResponseSchema,
@@ -10,184 +9,22 @@ import {
 import { GroundingDataSchema } from "../../entity/grounding";
 import { zodTypeGuard } from "../../utils/stdlib/type_guard";
 import { FillLocationSchema } from "./input";
-import { nonNullable } from "../../utils/stdlib/nullable";
 import { TaskPreparedSchema } from "../../entity/task";
-import { onFlow } from "@genkit-ai/firebase/functions";
-import { appAuthPolicy } from "../../utils/ai/authPolicy";
+import { LocationSchema } from "../../entity/location";
+import { queryLocation } from "../../utils/queryLocation";
 
 export const ResponseSchema = z.union([
   DataResponseSchema.extend({
     data: z.object({
-      result: z.literal("OK"),
+      task: TaskPreparedSchema.extend({
+        locations: z.array(LocationSchema),
+        locationsAITextResponse: z.string(),
+        locationsGroundings: z.array(GroundingDataSchema),
+      }),
     }),
   }),
   ErrorResponseSchema,
 ]);
-
-const TODOLocationSchema = z.object({
-  aiTextResponse: z.string(),
-  groundings: z.array(GroundingDataSchema),
-  locationName: z.string().nullable(),
-  address: z.string().nullable(),
-  email: z.string().nullable(),
-  tel: z.string().nullable(),
-});
-
-const todoLocation = genkitAI.defineTool(
-  {
-    name: "todoLocation",
-    description: "todoLocation",
-    inputSchema: z.object({
-      todo: z.object({
-        content: z.string(),
-        supplement: z.string().nullish(),
-      }),
-      taskQuestion: z.string(),
-      userLocation: z.object({
-        name: z.string(),
-        latitude: z.number(),
-        longitude: z.number(),
-      }),
-    }),
-    outputSchema: TODOLocationSchema,
-  },
-  async (input) => {
-    const { todo, taskQuestion, userLocation } = input;
-
-    const { aiTextResponse, groundings } = await googleSearchGroundingData(
-      `
-        「${userLocation.name}」から最寄りの 「${taskQuestion}」に関する 「${todo.content}。${todo.supplement ?? ""}」 に対応してくれる場所を出力してください。
-        出力はmarkdown形式にしてください
-        以下の情報を取得してください
-        - 場所の名前
-        - 場所の住所
-        - 場所のEmail
-        - 場所の電話番号
-        `
-    );
-
-    const todoLocationRAGSchema = TODOLocationSchema.omit({
-      aiTextResponse: true,
-      groundings: true,
-    });
-    const todoLocationRAGResponse = await genkitAI.generate({
-      prompt: `
-      次のマークダウンからスキーマに定義されている情報を抜き出してください
-      ${aiTextResponse}
-      `,
-      output: {
-        schema: todoLocationRAGSchema,
-      },
-    });
-    const output = todoLocationRAGResponse?.output;
-
-    const response: z.infer<typeof TODOLocationSchema> = {
-      locationName: output?.locationName ?? null,
-      address: output?.address ?? null,
-      email: output?.email ?? null,
-      tel: output?.tel ?? null,
-      aiTextResponse,
-      groundings,
-    };
-    return response;
-  }
-);
-
-// このflowはCloud Taskから使用されるのでエラーハンドリングは慎重に
-export const fillTODOLocation = genkitAI.defineFlow(
-  {
-    name: "fillTODOLocation",
-    inputSchema: FillLocationSchema,
-    outputSchema: ResponseSchema,
-  },
-  async (input) => {
-    console.log(`#fillTODOLocation: ${JSON.stringify({ input }, null, 2)}`);
-    try {
-      const {
-        taskID,
-        userLocation,
-        userRequest: { userID },
-      } = input;
-
-      const taskDocRef = database
-        .collection(`/users/${userID}/tasks`)
-        .doc(taskID);
-      const taskSnapshot = await taskDocRef.get();
-      const task = taskSnapshot.data();
-      if (!zodTypeGuard(TaskPreparedSchema, task)) {
-        return errorResponse(
-          new Error(`task loading parse error. taskLoading: ${task}`)
-        );
-      }
-
-      const todosCollectionRef = database.collection(
-        `/users/${userID}/tasks/${taskID}/todos`
-      );
-      const todoDocRefs = await todosCollectionRef.listDocuments();
-      const todoPromises = await Promise.allSettled(
-        todoDocRefs.map(async (todoDocRef) => {
-          const todoSnapshot = await todoDocRef.get();
-          const todo = todoSnapshot.data();
-          if (!zodTypeGuard(TODOSchema, todo)) {
-            // 変換できなかったものに関してはスルーする。位置情報等が埋まらないだけ
-            return null;
-          }
-          return todo;
-        })
-      );
-      const todos = todoPromises
-        .map((todoPromise) =>
-          todoPromise.status === "fulfilled" ? todoPromise.value : null
-        )
-        .filter(nonNullable);
-
-      for (const todo of todos) {
-        const {
-          aiTextResponse,
-          groundings,
-          locationName,
-          address,
-          email,
-          tel,
-        } = await todoLocation({
-          todo,
-          taskQuestion: task.question,
-          userLocation,
-        });
-
-        await database
-          .collection(`/users/${userID}/tasks/${taskID}/todos`)
-          .doc(todo.id)
-          .set(
-            {
-              ...todo,
-              location: {
-                aiTextResponse,
-                groundings,
-                name: locationName,
-                address,
-                email,
-                tel,
-              },
-            },
-            { merge: true }
-          );
-      }
-
-      const response: z.infer<typeof ResponseSchema> = {
-        result: "OK",
-        statusCode: 200,
-        data: {
-          result: "OK",
-        },
-      };
-
-      return response;
-    } catch (error) {
-      return errorResponse(error);
-    }
-  }
-);
 
 export const fillTaskLocation = genkitAI.defineFlow(
   {
@@ -215,6 +52,26 @@ export const fillTaskLocation = genkitAI.defineFlow(
           new Error(`task loading parse error. taskLoading: ${task}`)
         );
       }
+
+      // e.g) 杉並区成田東4丁目周辺 確定申告に関しての問い合わせ先を教えてください「申告方法の選択: e-Taxを利用するか、郵送または税務署への持参するか決定します
+      const { aiTextResponse, groundings, locations } = await queryLocation({
+        query: `${userLocation.name}周辺 ${task.question}に関しての問い合わせ先を教えてください`,
+      });
+
+      const response: z.infer<typeof ResponseSchema> = {
+        result: "OK",
+        statusCode: 200,
+        data: {
+          task: {
+            ...task,
+            locations: locations,
+            locationsAITextResponse: aiTextResponse,
+            locationsGroundings: groundings,
+          },
+        },
+      };
+
+      return response;
     } catch (error) {
       return errorResponse(error);
     }
